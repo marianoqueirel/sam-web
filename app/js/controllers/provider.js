6affd03fad1936b7bbe4819dc9d72b55edbc5d68
Adrian Patania <adrianpatania@gmail.com>
2017-09-10 20:45:26 -0300

    WIP

--- app/js/controllers/provider.js
+++ app/js/controllers/provider.js
@@ -4,6 +4,5 @@ angular.module('payeSAM.controllers')
   .controller('ProviderCtrl', ['$rootScope', '$scope', '$location', '$window', 'Provider',
     function ($rootScope, $scope, $location, $window, Provider) {
       $scope.init = function () {
-        $scope.status = 'ANDA';
       };
   }]);
