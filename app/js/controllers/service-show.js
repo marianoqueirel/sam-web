2bc0419a341b73ef4e3be4c5b168b0aef04024e5
Leandro Rodriguez <learod17@gmail.com>
2017-11-13 17:52:12 -0300

    WIP - list audits in services

--- app/js/controllers/service-show.js
+++ app/js/controllers/service-show.js
@@ -32,6 +32,27 @@ angular.module('payeSAM.controllers')
           { id: service_id },
           function (data) {
             $scope.service = data;
+            $scope.service.place = 'Un Lugar en el mundo';
+            $scope.service.diagnosis = 'Un Lugar diagnostico';
+            $scope.service.audits = [
+              {
+                'approved': true,
+                'started_on': moment().format('DD-MM-YYY'),
+                'days': 15,
+                'provider_name': 'Proveedor 1',
+                'service_type_option': 'Nivel I',
+                'auditor_name': 'Auditor 1'
+              },
+              {
+                'approved': false,
+                'started_on': moment().format('DD-MM-YYY'),
+                'days': 15,
+                'provider_name': 'Proveedor 2',
+                'service_type_option': 'Nivel II',
+                'auditor_name': 'Auditor 2'
+
+              }
+            ];
           }
         );
       }
